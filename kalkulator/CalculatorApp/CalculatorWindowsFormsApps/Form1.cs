80de2d7b759f2af897dca09c5377b63491f9a933
Packooo <you@example.com>
2023-10-06 20:58:50 +0700

    Add project files.

---
+++ kalkulator/CalculatorApp/CalculatorWindowsFormsApps/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorWindowsFormsApps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        private int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        private int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+
+        private int Pembagian(int a, int b)
+        {
+            return a / b;
+        }
+
+        private void btnHitung_Click(object sender, EventArgs e)
+        {
+            var a = int.Parse(txtNilaiA.Text); //casting
+            var b = Convert.ToInt32(txtNilaiB.Text); //casting
+
+            lstHasil.Items.Clear(); //kosongkan isi listbox
+
+            //tampilkan hasil semua hasil pemanggilan methode kalkulator ke dalam listbox
+            lstHasil.Items.Add(string.Format("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a,b)));
+            lstHasil.Items.Add(string.Format("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a,b)));
+            lstHasil.Items.Add(string.Format("Hasil Perkalian: {0} x {1} = {2}", a, b, Perkalian(a,b)));
+            lstHasil.Items.Add(string.Format("Hasil Pembagian: {0} / {1} = {2}", a, b, Pembagian(a,b)));
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.comboBox1.Items.Clear();
+            this.comboBox1.Items.AddRange(new object[] {"Penambahan",
+                        "Pengurangan",
+                        "Perkalian",
+                        "Pembagian"
+            });
+            if (this.comboBox1.SelectedItem == "Penambahan")
+            {
+                this.comboBox1.Items.Add(string.Format("Penambahan"));
+            }
+        }
+    }
+}
---
+++ kalkulator/KalkulatorConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalkulatorConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 6;
+
+            var cal = new Calculator();
+            Console.WriteLine(a + " + " + b + " = " + cal.Tambah(a, b));
+            Console.WriteLine(a + " - " + b + " = " + cal.Kurang(a, b));
+
+            Console.ReadKey();
+        }
+    }
+
+    class Calculator
+    {
+        public int Tambah(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Kurang(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
