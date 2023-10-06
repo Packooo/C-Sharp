66c8845d6ad5539d6ec1c371544f073385508b42
Packooo <you@example.com>
2023-10-06 21:31:30 +0700

    Commit pertama kalkulator sederhana

---
+++ Kalkulator Sederhana/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Kalkulator_Sederhana
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNomorA = new System.Windows.Forms.TextBox();
+            this.txtNomorB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblHasil = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nomor A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nomor B";
+            // 
+            // txtNomorA
+            // 
+            this.txtNomorA.Location = new System.Drawing.Point(107, 40);
+            this.txtNomorA.Name = "txtNomorA";
+            this.txtNomorA.Size = new System.Drawing.Size(158, 20);
+            this.txtNomorA.TabIndex = 2;
+            // 
+            // txtNomorB
+            // 
+            this.txtNomorB.Location = new System.Drawing.Point(107, 74);
+            this.txtNomorB.Name = "txtNomorB";
+            this.txtNomorB.Size = new System.Drawing.Size(158, 20);
+            this.txtNomorB.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hasil";
+            // 
+            // lblHasil
+            // 
+            this.lblHasil.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblHasil.Location = new System.Drawing.Point(107, 120);
+            this.lblHasil.Name = "lblHasil";
+            this.lblHasil.Size = new System.Drawing.Size(161, 34);
+            this.lblHasil.TabIndex = 5;
+            this.lblHasil.Text = "lblHasil";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(46, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 62);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(46, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 62);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(152, 166);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 62);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(152, 250);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 62);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(273, 166);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 146);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "C";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 340);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblHasil);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNomorB);
+            this.Controls.Add(this.txtNomorA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "KALKULATOR SEDERHANA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNomorA;
+        private System.Windows.Forms.TextBox txtNomorB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblHasil;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Kalkulator Sederhana/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator_Sederhana
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtNomorA.Clear();
+            txtNomorB.Clear();
+            lblHasil.Text = string.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtNomorA.Text) || string.IsNullOrEmpty(txtNomorB.Text))
+            {
+                MessageBox.Show("Nomor A atau Nomor B Tidak Boleh Kosong!!");
+            } else
+            {
+                int a = int.Parse(txtNomorA.Text);
+                int b = int.Parse(txtNomorB.Text);
+                int c = a + b;
+                lblHasil.Text = c.ToString();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txtNomorA.Clear();
+            txtNomorB.Clear();
+            lblHasil.Text = string.Empty;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNomorA.Text) || string.IsNullOrEmpty(txtNomorB.Text))
+            {
+                MessageBox.Show("Nomor A atau Nomor B Tidak Boleh Kosong!!");
+            }
+            else
+            {
+                int a = int.Parse(txtNomorA.Text);
+                int b = int.Parse(txtNomorB.Text);
+                int c = a - b;
+                lblHasil.Text = c.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNomorA.Text) || string.IsNullOrEmpty(txtNomorB.Text))
+            {
+                MessageBox.Show("Nomor A atau Nomor B Tidak Boleh Kosong!!");
+            }
+            else
+            {
+                int a = int.Parse(txtNomorA.Text);
+                int b = int.Parse(txtNomorB.Text);
+                int c = a * b;
+                lblHasil.Text = c.ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNomorA.Text) || string.IsNullOrEmpty(txtNomorB.Text))
+            {
+                MessageBox.Show("Nomor A atau Nomor B Tidak Boleh Kosong!!");
+            }
+            else
+            {
+                int a = int.Parse(txtNomorA.Text);
+                int b = int.Parse(txtNomorB.Text);
+                int c = a / b;
+                lblHasil.Text = c.ToString();
+            }
+        }
+    }
+}
